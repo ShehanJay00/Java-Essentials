39754c29cc77e67aa596a97115c72c88451a277e
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2025-08-15 21:37:31 +0530

    ForEach

---
+++ demo/first/src/ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach {
+    public static void main(String[] args) {
+        //Used exclusively to loop through elements in an array (or other data structures)
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
