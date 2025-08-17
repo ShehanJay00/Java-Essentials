78bbbc9ba9420e9c78e63591dad9a21bdbb51ce2
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2025-08-17 14:29:06 +0530

    Nested Classes

---
+++ demo/first/src/Enum.java
@@ -0,0 +1,14 @@
+import javax.xml.transform.stream.StreamSource;
+
+public class Enum {
+
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
