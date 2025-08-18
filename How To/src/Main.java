246df8bb0adad0778f85d24f9a89ff8ffcbc7890
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2025-08-18 13:39:12 +0530

    How to Solve

---
+++ How To/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println(" ");
+
+        //1
+        String words = "GR Shehan Malinda Jayasooriya";
+        int countWords = words.split(" ").length;
+        System.out.println(countWords);
+
+        System.out.println(" ");
+
+        //2
+
+
+
+
+    }
+}
